1de19d34b498c391af69224dabd23a7cdec8f1e2
Efrain May <efrain_may_75@hotmail.com>
2021-01-19 14:26:36 -0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/bandnamesapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bandnamesapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,52 @@
+import 'package:bandnamesapp/models/band.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+  List<Band> bands = [
+    Band(id: '1', name: 'Metallica', votes: 5),
+    Band(id: '2', name: 'Queen', votes: 1),
+    Band(id: '4', name: 'Strike 3', votes: 4),
+    Band(id: '5', name: 'Alain Perez', votes: 7),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BandNames', style: TextStyle(color: Colors.black87)),
+        backgroundColor: Colors.white,
+        elevation: 1,
+      ),
+      body: ListView.builder(
+        itemCount: bands.length,
+        itemBuilder: ( context, i) => _bandTile(bands[i]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        elevation: 1,
+        onPressed: (){},
+      ),
+    );
+  }
+
+  ListTile _bandTile(Band band) {
+    return ListTile(
+      leading: CircleAvatar(
+        child: Text(band.name.substring(0,2)),
+        backgroundColor: Colors.blue[100],
+      ),
+      title: Text(band.name),
+      trailing: Text('${band.votes}', style: TextStyle(fontSize: 20),),
+      onTap: (){
+        print(band.name);
+      },
+    );
+  }
+}
